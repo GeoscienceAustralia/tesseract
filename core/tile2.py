9aa7881641b29513897877ce49854274a14f6ec6
MonkeyButter <p.rozas.larraondo@gmail.com>
2015-02-04 14:35:04 +1100

    index tiles reimplemented

--- core/tile2.py
+++ core/tile2.py
@@ -24,12 +24,12 @@ def load_partial_tile(item, lat_start, lat_end, lon_start, lon_end, lazy=True):
 class Tile2(object):
 
     # Consider integrating satellite information inside id_object
-    def __init__(self, sat=None, origin_id=None, bands=None, lat_start=None, lat_end=None,
-                 lon_start=None, lon_end=None, lazy=True):
+    def __init__(self, origin_id=None, bands=None, lat_start=None, lat_end=None,
+                 lon_start=None, lon_end=None, array =None, lazy=True):
 
         self.origin_id = origin_id
         # TODO Hardcoded satellite (Should come with origin_id)
-        self.origin_id["satellite"] = sat="LS5_TM"
+        self.origin_id["satellite"] = "LS5_TM"
 
         orig_y_dim = get_geo_dim(origin_id["lat_start"], origin_id["lat_extent"], origin_id["pixel_size"])
         orig_x_dim = get_geo_dim(origin_id["lon_start"], origin_id["lon_extent"], origin_id["pixel_size"])
@@ -62,52 +62,25 @@ class Tile2(object):
         # TODO: Properly implement band dimension
         if len(index) == 2:
 
-            #Mostly sure about comparisons
-            #lat_bounds = self._y_dim[0] <= index[0].start <= self._y_dim[-1] or self._y_dim[0] < index[0].stop < self._y_dim[-1]
-            #lon_bounds = self._x_dim[0] <= index[1].start <= self._x_dim[-1] or self._x_dim[0] < index[1].stop < self._x_dim[-1]
-            lat_bounds = index[0].start <= self.y_dim[-1] and index[0].stop > self.y_dim[0]
-            lon_bounds = index[1].start <= self.x_dim[-1] and index[1].stop > self.x_dim[0]
+            corr_lat_start = filter_coord(index[0].start, self.y_dim)
+            corr_lat_end = filter_coord(index[0].end, self.y_dim)
+            corr_lon_start = filter_coord(index[1].start, self.x_dim)
+            corr_lon_end = filter_coord(index[1].end, self.x_dim)
 
-            bounds = (lat_bounds, lon_bounds)
-
-            if bounds.count(True) == len(bounds):
-
-                start_lat_index = max(index[0].start, self.y_dim[0])
-                array_lat_start_index = np.abs(self.y_dim - index[0].start).argmin()
-                start_lon_index = max(index[1].start, self.x_dim[0])
-                array_lon_start_index = np.abs(self.x_dim - index[1].start).argmin()
-
-                if index[0].stop > np.max(self.y_dim):
-                    end_lat_index = np.max(self.y_dim) + self.origin_id["pixel_size"]
-                    array_lat_end_index = None
-
-                else:
-                    end_lat_index = index[0].stop
-                    array_lat_end_index = np.abs(self.y_dim - index[0].stop).argmin()
-
-                if index[1].stop > np.max(self.x_dim):
-                    end_lon_index = np.max(self.x_dim) + self.origin_id["pixel_size"]
-                    array_lon_end_index = None
-
-                else:
-                    end_lon_index = index[1].stop
-                    array_lon_end_index = np.abs(self.x_dim - index[1].stop).argmin()
+            lat1 = get_index(corr_lat_start, self.y_dim)
+            lat2 = get_index(corr_lat_end, self.y_dim) + 1
+            lon1 = get_index(corr_lon_start, self.x_dim)
+            lon2 = get_index(corr_lon_end, self.x_dim) + 1
 
 
-                if self._array is None:
-                    return Tile2(self._sat, self._prod, self._lat_id, self._lon_id, self._time, self._pixel_size,
-                                len(self._band_dim), start_lat_index, start_lon_index,
-                                end_lat_index-start_lat_index, end_lon_index-start_lon_index, None)
-
-                else:
-                    return Tile2(self._sat, self._prod, self._lat_id, self._lon_id, self._time, self._pixel_size,
-                                len(self._band_dim), start_lat_index, start_lon_index,
-                                end_lat_index-start_lat_index, end_lon_index-start_lon_index, 
-                                self._array[array_lon_start_index:array_lon_end_index,
-                                array_lat_start_index:array_lat_end_index])
+            if self.array is None:
+                return Tile2(origin_id=self.origin_id, bands=None, lat_start=corr_lat_start, lat_end=corr_lat_end,
+                 lon_start=corr_lon_start, lon_end=corr_lon_end, array=None, lazy=True)
 
             else:
-                return None
+                return Tile2(origin_id=self.origin_id, bands=None, lat_start=corr_lat_start, lat_end=corr_lat_end,
+                 lon_start=corr_lon_start, lon_end=corr_lon_end, array=self.array[lon1:lon2, lat1:lat2], lazy=True)
+
         else:
             # TODO: Properly manage index exceptions
             raise Exception
