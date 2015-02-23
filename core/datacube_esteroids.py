5586a7fce8aa048756a8d5d77a26cb2b1b1e2baa
monkeybutter <p.rozas.larraondo@gmail.com>
2015-02-23 13:06:24 +1100

    New datacube v3.0.6

--- core/datacube_esteroids.py
+++ core/datacube_esteroids.py
@@ -123,4 +123,4 @@ if __name__ == "__main__":
 
     df = df_fc.join(df_wofs)
     #print df.to_json(date_format='iso', orient='records')
-    print df.dropna(how='any', inplace=True).head(5)
+    print df.dropna(axis=1, how='any', inplace=True).head(5)
