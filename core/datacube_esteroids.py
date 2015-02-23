1e865c966b34bd0f08f88584df57b4b9a6fb0328
MonkeyButter <p.rozas.larraondo@gmail.com>
2015-02-22 19:00:18 -0500

    New datacube implementation

---
+++ core/datacube_esteroids.py
@@ -0,0 +1,147 @@
+import math
+import time
+from datetime import datetime
+import h5py
+import os.path
+import numpy as np
+import pandas as pd
+import argparse
+
+abs_path = "/g/data1/v10/HPCData/test/"
+
+class Datacube(object):
+    # Consider integrating satellite information inside id_object
+    def __init__(self, t_dim=None, x_dim=None, y_dim=None, b_dim=None, array=None):
+        
+        self.t_dim = t_dim
+        self.x_dim = x_dim
+        self.y_dim = y_dim
+        self.b_dim = b_dim
+        self.array = array
+     
+        
+def create_datacube(product=None, t1=None, t2=None, x1=None, x2=None, y1=None, y2=None, bands=None):
+    # Consider integrating satellite information inside id_object
+        
+    file_names = _indexer(abs_path, product, t1, t2, x1, x2, y1, y2)
+
+    cubes = []
+
+    for file_name in file_names:
+        
+        if os.path.isfile(file_name):
+
+            a = Datacube()
+
+            with h5py.File(file_name, 'r') as afile:
+                if len(afile[product].shape) == 3:
+                    time_dim = afile[product].dims[0][0].value
+                    x_dim = afile[product].dims[1][0].value
+                    y_dim = afile[product].dims[2][0].value
+
+                    t1_i = get_index(time.mktime(t1.timetuple()), time_dim)
+                    t2_i = get_index(time.mktime(t2.timetuple()), time_dim)
+                    a.t_dim = time_dim[t1_i:t2_i]
+
+                    x1_i = get_index(x1, x_dim)
+                    x2_i = get_index(x2, x_dim)
+                    a.x_dim = x_dim[x1_i:x2_i]
+
+                    y1_i = get_index(y1, y_dim)
+                    y2_i = get_index(y2, y_dim)
+                    a.y_dim = y_dim[y1_i:y2_i]
+
+                    a.array = afile[product][t1_i:t2_i, x1_i:x2_i, y1_i:y2_i]
+
+                elif len(afile[product].shape) == 4: 
+                    time_dim = afile[product].dims[0][0].value
+                    x_dim = afile[product].dims[1][0].value
+                    y_dim = afile[product].dims[2][0].value
+                    band_dim = afile[product].dims[3][0].value    
+
+                    t1_i = get_index(time.mktime(t1.timetuple()), time_dim)
+                    t2_i = get_index(time.mktime(t2.timetuple()), time_dim)
+                    a.t_dim = time_dim[t1_i:t2_i]
+
+                    x1_i = get_index(x1, x_dim)
+                    x2_i = get_index(x2, x_dim)
+                    a.x_dim = x_dim[x1_i:x2_i]
+
+                    y1_i = get_index(y1, y_dim)
+                    y2_i = get_index(y2, y_dim)
+                    a.y_dim = y_dim[y1_i:y2_i]
+
+                    a.b_dim = band_dim
+
+                    a.array = afile[product][t1_i:t2_i, x1_i:x2_i, y1_i:y2_i, :]
+            
+            cubes.append(a)
+
+    # Here comes the function to merge the cubes  
+
+    return cubes
+
+
+def _indexer(root=None, product=None, t1=None, t2=None, x1=None, x2=None, y1=None, y2=None):
+    files = []
+    
+    for year in range(t1.year, t2.year+1):
+        for x in range(int(math.floor(x1)), int(math.floor(x2))+1):
+            for y in range(int(math.floor(y1)), int(math.floor(y2))+1):
+                files.append(root + "LS5_TM_{0}_{1}_{2:04d}_{3}.h5".format(product, x, y, year))
+ 
+    return files
+
+
+def get_index(value, dimension):
+    return np.abs(dimension-value).argmin()
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="""Pixel Drill argument parser""")
+    
+    parser.add_argument(dest="start_date", type=str, help="Pixel drill start date.")
+    parser.add_argument(dest="end_date", type=str, help="Pixel drill end date.")
+    parser.add_argument(dest="start_x", type=float, help="Pixel drill start x.")
+    parser.add_argument(dest="start_y", type=float, help="Pixel drill start y.")
+    parser.add_argument("-b", "--bands", dest="bands", type=int, default=1, help="Pixel drill bands span.")
+
+    args = parser.parse_args()
+        
+    #time1 = datetime.strptime("1985-08-01T00:00:00.000Z", '%Y-%m-%dT%H:%M:%S.%fZ')
+    #time2 = datetime.strptime("2016-09-01T00:00:00.000Z", '%Y-%m-%dT%H:%M:%S.%fZ')
+    time1 = datetime.strptime(args.start_date, '%Y-%m-%dT%H:%M:%S.%fZ')
+    time2 = datetime.strptime(args.end_date, '%Y-%m-%dT%H:%M:%S.%fZ')
+
+    cubes = create_datacube(product="WOFS", t1=time1, t2=time2, x1=args.start_x, x2=args.start_x+.00025, y1=args.start_y, y2=args.start_y+.00025)
+
+    dfs = []
+    for cube in cubes:
+        index = map(datetime.fromtimestamp, cube.t_dim)
+        dfs.append(pd.DataFrame(np.squeeze(cube.array), index=index, columns=["E"]))
+        #dfs.append(pd.DataFrame.from_records(np.squeeze(cube.array), index=index, columns=["A"]))
+   
+    df_wofs = pd.concat(dfs)
+    df_wofs["index"] = df_wofs.index 
+    df_wofs = df_wofs.drop_duplicates(cols='index')
+    df_wofs = df_wofs.drop("index", 1)
+    #print df_wofs.to_json(date_format='iso')
+    print df_wofs.shape
+
+    cubes = create_datacube(product="FC", t1=time1, t2=time2, x1=args.start_x, x2=args.start_x+.00025, y1=args.start_y, y2=args.start_y+.00025)
+    
+    dfs = []
+    for cube in cubes:
+        index = map(datetime.fromtimestamp, cube.t_dim)
+        dfs.append(pd.DataFrame.from_records(np.squeeze(cube.array), index=index, columns=["A", "B", "C", "D"]))
+    
+    df_fc = pd.concat(dfs)
+    df_fc["index"] = df_fc.index 
+    df_fc = df_fc.drop_duplicates(cols='index')
+    df_fc = df_fc.drop("index", 1)
+    #print df_fc.to_json(date_format='iso', orient='records')
+    print df_fc.shape
+
+    df = df_fc.join(df_wofs)
+    print df.to_json(date_format='iso', orient='records')
+    print df.shape
