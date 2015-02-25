3d38480ce90a5d6a7ea6ce5ab9b8c4f301ce0612
monkeybutter <p.rozas.larraondo@gmail.com>
2015-02-25 16:40:52 +1100

    Angular D3 directives implemented

--- web/server.go
+++ web/server.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"encoding/json"
+    "log"
+    "encoding/json"
 	"github.com/gorilla/mux"
 	"net/http"
 	"os/exec"
@@ -17,9 +18,63 @@ func projectAll(w http.ResponseWriter, r *http.Request) {
 
 	//var filePath = HomeFolder + year + "/" + exp_name + "/" + source + "/" + date + "/"
 
-	var result = exec.Command("python", "../core/datacube_esteroids.py", "1985-08-01T00:00:00.000Z",
-	"2000-09-01T00:00:00.000Z", "147.542", "-30.6234").Run()
-	jsonBytes, _ := json.Marshal(result)
+	out, err := exec.Command("python", "../core/datacube_esteroids.py", "1985-08-01T00:00:00.000Z", "2000-09-01T00:00:00.000Z", "147.542", "-30.6234").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(out)
+}
+
+
+func projectAll1(w http.ResponseWriter, r *http.Request) {
+
+    x := make([]interface{}, 0)
+    geomap1 := make(map[string]interface{})
+    geomap1["date"] = "11-Oct-13"
+    geomap1["IE"] = 41.62
+    geomap1["Chrome"] = 22.36
+    geomap1["Firefox"] = 25.58
+    geomap1["Safari"] = 9.13
+    geomap1["Opera"] = 1.22
+    x = append(x, geomap1)
+    geomap2 := make(map[string]interface{})
+    geomap2["date"] = "11-Oct-14"
+    geomap2["IE"] = 41.95
+    geomap2["Chrome"] = 22.15
+    geomap2["Firefox"] = 25.78
+    geomap2["Safari"] = 8.79
+    geomap2["Opera"] = 1.25
+    x = append(x, geomap2)
+    geomap3 := make(map[string]interface{})
+    geomap3["date"] = "11-Oct-15"
+    geomap3["IE"] = 37.64
+    geomap3["Chrome"] = 24.77
+    geomap3["Firefox"] = 25.96
+    geomap3["Safari"] = 10.16
+    geomap3["Opera"] = 1.39
+    x = append(x, geomap3)
+    geomap4 := make(map[string]interface{})
+    geomap4["date"] = "11-Oct-16"
+    geomap4["IE"] = 37.64
+    geomap4["Chrome"] = 24.65
+    geomap4["Firefox"] = 25.98
+    geomap4["Safari"] = 10.59
+    geomap4["Opera"] = 1.44
+    x = append(x, geomap4)
+
+    //x1 := []int{3, 6, 3}
+
+    jsonBytes, _ := json.Marshal(x)
+
+	w.Write([]byte(jsonBytes))
+}
+
+func projectAll2(w http.ResponseWriter, r *http.Request) {
+
+    x := []int{3, 6, 3}
+
+	jsonBytes, _ := json.Marshal(x)
+
 	w.Write([]byte(jsonBytes))
 }
 
@@ -27,10 +82,12 @@ func main() {
 	r := mux.NewRouter()
 
 	r.Handle("/", http.RedirectHandler("/web_server/index.html", 302))
-	r.HandleFunc("/pixel_drill/{year:[0-9]+}/{exp_name:[A-Za-z0-9._ %-]+}/{source:[A-Za-z0-9._ %-]+}/{date:[0-9]+}/", projectAll).Methods("GET")
+	r.HandleFunc("/pixel_drill/{year:[0-9]+}/{exp_name:[A-Za-z0-9._ %-]+}/{source:[A-Za-z0-9._ %-]+}/{date:[0-9]+}/", projectAll2).Methods("GET")
+	r.HandleFunc("/pixel_drull/{year:[0-9]+}/{exp_name:[A-Za-z0-9._ %-]+}/{source:[A-Za-z0-9._ %-]+}/{date:[0-9]+}/", projectAll1).Methods("GET")
+
 	r.PathPrefix("/web_server/").Handler(http.StripPrefix("/web_server", http.FileServer(http.Dir("./static/"))))
 
 	http.Handle("/", r)
 
-	panic(http.ListenAndServe(":8081", nil))
+	panic(http.ListenAndServe("127.0.0.1:8081", nil))
 }
--- web/static/js/script.js
+++ web/static/js/script.js
@@ -3,44 +3,172 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('MainCtrl', function($scope, $http){
-// using `then()` callback style
-$http.get('donut-data-2.json').then(function(response){
-$scope.donutData2 = response.data;
-}, function(err){
-throw err;
+
+    $scope.data = null;
+    $scope.dato = null;
+
+    $http({
+        method: 'GET',
+        url: "/pixel_drill/23/23/23/23/",
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    }).success(function(response){
+        $scope.data = response;
+        console.log("response data: " + $scope.data)
+
+    }).error(function(){
+        alert("Error ");
+    });
+
+    $http({
+        method: 'GET',
+        url: "/pixel_drull/23/23/23/23/",
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    }).success(function(response){
+        var parseDate = d3.time.format("%y-%b-%d").parse
+        response.forEach(function(d) {
+            d.date = parseDate(d.date);
+        });
+        $scope.dato = response;
+        console.log("response dato: " + $scope.dato)
+
+    }).error(function(){
+        alert("Error ");
+    });
+
 });
-});
 
-myApp.directive('donutChart', function(){
-  function link(scope, element, attr){
-    var color = d3.scale.category10();
-    var data = scope.data;
-    var width = 300;
-    var height = 300;
-    var min = Math.min(width, height);
-    var svg = d3.select('body').append('svg');
-    var pie = d3.layout.pie().sort(null);
-    var arc = d3.svg.arc()
-                    .outerRadius(min / 2 * 0.9)
-                    .innerRadius(min / 2 * 0.5);
 
-    svg.attr({width: width, height: height});
+myApp.directive('areaChart', function(){
 
-    var g = svg.append('g')
-    // center the donut chart
-    .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+  function link(scope, el, attr){
 
-    // add the <path>s for each arc slice
-    g.selectAll('path').data(pie(data))
-                       .enter().append('path')
-                       .style('stroke', 'white')
-                       .attr('d', arc)
-                       .attr('fill', function(d, i){ return color(i) });
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+    var formatPercent = d3.format(".0%");
+
+    var x = d3.time.scale()
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var color = d3.scale.category20();
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickFormat(formatPercent);
+
+    var area = d3.svg.area()
+        .x(function(d) { return x(d.date); })
+        .y0(function(d) { return y(d.y0); })
+        .y1(function(d) { return y(d.y0 + d.y); });
+
+    var stack = d3.layout.stack()
+        .values(function(d) { return d.values; });
+
+    var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+    scope.$watch('data', function(data){
+      console.log('changed dato: ' + data)
+      if(!data){
+        return;
+      }
+      var browsers = stack(color.domain().map(function(name) {
+        return {
+          name: name,
+          values: data.map(function(d) {
+            return {date: d.date, y: d[name] / 100};
+          })
+        };
+        console.log(browsers)
+      }));
+
+      x.domain(d3.extent(data, function(d) { return d.date; }));
+
+      var browser = svg.selectAll(".browser")
+          .data(browsers)
+        .enter().append("g")
+          .attr("class", "browser");
+
+      browser.append("path")
+          .attr("class", "area")
+          .attr("d", function(d) { return area(d.values); })
+          .style("fill", function(d) { return color(d.name); });
+
+      browser.append("text")
+          .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+          .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.y0 + d.value.y / 2) + ")"; })
+          .attr("x", -6)
+          .attr("dy", ".35em")
+          .text(function(d) { return d.name; });
+
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis);
+
+    }, true);
 
   }
   return {
     link: link,
     restrict: 'E',
     scope: { data: '=' }
+  };
+});
+
+myApp.directive('donutChart', function(){
+  function link(scope, el, attr){
+    var color = d3.scale.category10();
+    var width = 300;
+    var height = 300;
+    var min = Math.min(width, height);
+    var svg = d3.select(el[0]).append('svg');
+    var pie = d3.layout.pie().sort(null);
+    var arc = d3.svg.arc()
+      .outerRadius(min / 2 * 0.9)
+      .innerRadius(min / 2 * 0.5);
+
+    svg.attr({width: width, height: height});
+    // center the donut chart
+    var g = svg.append('g')
+      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
+    // add the <path>s for each arc slice
+    var arcs = g.selectAll('path');
+
+    scope.$watch('data', function(data){
+      console.log("changed data: " + data)
+      if(!data){ return; }
+      arcs = arcs.data(pie(data));
+      arcs.exit().remove();
+      arcs.enter().append('path')
+        .style('stroke', 'white')
+        .attr('fill', function(d, i){ return color(i) });
+      // update all the arcs (not just the ones that might have been added)
+      arcs.attr('d', arc);
+    }, true);
+
   }
+  return {
+    link: link,
+    restrict: 'E',
+    scope: { data: '=' }
+  };
 });
