341b221cf8bda5200f774c6ae9c358a2639186d3
monkeybutter <p.rozas.larraondo@gmail.com>
2015-03-11 14:03:28 +1100

    reverse dim v0.2

--- core/reverse_dim.py
+++ core/reverse_dim.py
@@ -1,3 +1,5 @@
 import h5py
 
-with h5py.File("/g/data/rs0/
+with h5py.File("/g/data/rs0/tiles/EPSG4326_1deg_0.00025pixel_netcdf/HPCData/ERA_INTERIM/TP_25-31_147-152_1985-2015.h5", 'r') as hfile:
+    y_dim = hfile["TP"].dims[1][0].value
+    print y_dim
