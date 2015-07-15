0f1caceb0f0830959b7c46c1a630bef851d43fc7
Josh Sixsmith <josh.sixsmith@gmail.com>
2015-07-15 22:55:24 +1000

    Added crs and crs_wkt attributes.

--- core/tessera2.py
+++ core/tessera2.py
@@ -25,15 +25,29 @@ class TesseraDatasets(object):
         self.fname = filename
         self.container = h5py.File(filename, 'r')
 
+        # Basic metadata info (timestamps for now)
         self.metadata = self.container['metadata']['timestamps']
         tmp_ = self.metadata[:].astype('datetime64[ns]')
         self.time_slices = tmp_.shape[0]
         self.timestamps = pandas.DataFrame({'idx': range(self.time_slices)},
                                            index=tmp_)
+
+        # Setup the CRS/SRS/Projection (for now just keep GDAL like
+        # 'projection' call. More recent libs use crs
         self.projection = self.metadata.attrs['projection']
+        self.crs_wkt = self.metadata.attrs['projection']
+        crs = osr.SpatialReference()
+        crs.ImportFromWkt(self.metadata.attrs['projection'])
+        self.crs = crs.ExportToProj4()
+        
+        # Setup the image tie point (just mimic the GDAL named 'geotransform'
         self.geotransform = tuple(self.metadata.attrs['geotransform'])
+
+        # Array dimensions y (lines/height), x (samples/width)
         self.lines = self.metadata.attrs['lines']
         self.samples = self.metadata.attrs['samples']
+
+        # Setup the datasets
         self.dsets = {}
         tmp_ = self.container['data']
         for key in self.container['data'].keys():
